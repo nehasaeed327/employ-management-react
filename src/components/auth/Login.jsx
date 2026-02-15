2d56691e63d5f46dba2f433ba75b0e2441ca6d73
Neha Saeed <nehasaeed327@gmail.com>
2026-02-15 16:19:13 +0500

    Updated Login page with 3D decoration and background image

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -16,16 +16,16 @@ const Login = () => {
     <div className="flex h-screen w-screen justify-center bg-[url('../../../public/bg.jpg')] bg-cover bg-center ">
       <div className="flex justify-center items-center w-[50vw] h-[100vh] bg-black" >
         <div className="border-full border-indigo-600 justify-center items-center p-20 border-2 rounded">
-          <form  onSubmit={(e)=>{
+          <form  onSubmit= {(e)=>{
               
               submitHandler(e)
-            }
+            }}
             
-            }   action="" className=" flex flex-col justify-center items-center">
+             className=" flex flex-col justify-center items-center">
             <input required value={email} onChange={ (e)=>{
               setEmail(e.target.value)
                         }} type="email" placeholder='Enter your email'
-            className=" w-80 border-2 py-4 px-5 bg -transparent border-indigo-600 rounded-full outline-none text-indigo-600  focus:bg-transparent placeholder:text-gray-400" />
+            className="w-80 border-2 py-4 px-5 bg-transparent border-indigo-600 rounded-full outline-none text-indigo-600  focus:bg-transparent placeholder:text-gray-400" />
               <input required 
                 value={password}
 
@@ -34,7 +34,7 @@ const Login = () => {
                 }}
               
               type="password" placeholder='Enter your password'
-            className=" w-80 border-2 py-4 px-5 bg -transparent border-indigo-600 rounded-full outline-none text-indigo-600 mt-3 placeholder:text-gray-400" />
+            className=" w-80 border-2 py-4 px-5 bg-transparent border-indigo-600 rounded-full outline-none text-indigo-600 mt-3 placeholder:text-gray-400" />
 
             <button type='submit' className="border-2 py-4 px-[40px] bg-indigo-600 border-indigo-600 rounded-full outline-none text-white mt-3 ">Login</button>
           </form>
